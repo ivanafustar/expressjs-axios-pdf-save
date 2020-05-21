9094fa8d9b0ea4d73cca404632a050fadcb69896
ivana <ivanafustar@hotmail.com>
2020-05-21 10:47:13 +0200

    - using arrow functions accross project - using const instead of var

--- routes/pdf.js
+++ routes/pdf.js
@@ -4,14 +4,14 @@ const downloadPdf = require('../services/pdf.service');
 const getData = require('../services/data.service');
 
 /* GET pdf */
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     const fileUrl = 'https://www.cpd.org.au/wp-content/uploads/2014/11/placeholder.pdf';
     const fileLoc = './my.pdf';
     const dataPath = 'https://jsonplaceholder.typicode.com/posts';
 
     downloadPdf(fileUrl, fileLoc)
         .then(() => getData(dataPath))
-        .then(function (response) {
+        .then((response) => {
             console.log(response.data)
 
             return res.status(200).send({
